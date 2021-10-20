10b488822e14b6b163633f58931d202a9a31a49c
Barkın Özakay <barkin.ozakay@kariyer.net>
2021-10-20 13:36:35 +0300

    cocoapods removed

--- Podfile
+++
@@ -1,6 +0,0 @@
-platform :ios, '9.0'
-
-target 'Book Discovery' do
-  use_frameworks!
-
-end
--- Pods/Target Support Files/Pods-Book Discovery/Pods-Book Discovery-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Book_Discovery : NSObject
-@end
-@implementation PodsDummy_Pods_Book_Discovery
-@end
